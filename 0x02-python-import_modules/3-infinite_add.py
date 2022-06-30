3893a386035d397a3bab89705c87e976e13147ed
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-06-30 22:43:28 +0000

    commit add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     from sys import argv
+
     sum = 0
-    for count in range(1, len(argv)):
-        sum += int(argv[count])
-    print("{}".format(sum))
+    for i, arg in enumerate(argv[1:]):
+        sum += int(arg)
+
+    print('{:d}'.format(sum))
