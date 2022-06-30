31031dac8941ec04d4e956b8f2f1f9424f2fb75f
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-06-30 22:27:06 +0000

    commit infinit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
-
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    from sys import argv
+    sum = 0
+    for count in range(1, len(argv)):
+        sum += int(argv[count])
+    print("{}".format(sum))
