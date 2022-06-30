de525909f6088b3f6894a15d5ae071ab639e79bd
Innocent Chukwuma <ichukwuma40@gmail.com>
2022-06-30 22:58:24 +0000

    commit variable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     from sys import argv
-    argc = len(argv)
-    if argc < 2:
-        print("{} arguments.".format(argc - 1))
+
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
+        print('{:d} argument:'.format(len(argv) - 1))
     else:
-        if argc == 2:
-            print("{} argument:".format(argc - 1))
-        else:
-            print("{} arguments:".format(argc - 1))
-    for n in range(1, argc):
-        print("{}: {}".format(n, argv[n]))
+        print('{:d} arguments:'.format(len(argv) - 1))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print('{:d}: {:s}'.format(i, arg))
