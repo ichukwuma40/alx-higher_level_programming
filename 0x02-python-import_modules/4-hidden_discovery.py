f292b31f7378debbf350e3f4715c69f0ba152a5f
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-09-07 17:39:35 -0700

    import modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_
-   
-   names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
-
+    import hidden_4
+    dir(hidden_4)
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print(i)
