615f2c497e6bfc7bbc225576914f4b04af7db3e2
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-11-16 10:39:53 -0800

    sql more queries

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- It creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- It creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- This use a database
+USE hbtn_0d_usa;
+-- To create a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
