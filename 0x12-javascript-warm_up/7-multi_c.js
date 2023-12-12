6f83f6d51aa48827632d144b63b1496b24642dbc
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-12-12 03:28:15 -0800

    javascript

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const lang = 'C is fun';
+
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
   }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+// prints a square
+
+if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
-    i++;
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
