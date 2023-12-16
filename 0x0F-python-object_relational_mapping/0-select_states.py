88d96f01bdad35783bf3819cd5871c75fd403712
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-12-16 12:47:38 -0800

    relational mappimg

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,18 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-	    id INT NOT NULL AUTO_INCREMENT, 
-	    name VARCHAR(256) NOT NULL,
-	    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+"""
+Lists all states from the database
+It takes 3 arguments: mysql username, mysql password and database name
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
