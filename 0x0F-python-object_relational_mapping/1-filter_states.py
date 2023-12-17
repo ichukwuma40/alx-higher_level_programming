2cb147f50449dda051f1b669b569966bbfad6db5
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-12-16 23:51:48 -0800

    object mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (upper N)
-It takes 3 arguments: mysql username, mysql password and database name
+Script that lists all states with a name starting with N (upper N)
+from the database
 """
-
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    query = "SELECT * FROM states\
-             WHERE states.name LIKE BINARY 'N%'\
-             ORDER BY states.id ASC"
-    cursor = db.cursor()
-    cursor.execute(query)
-    for state in cursor.fetchall():
-        print(state)
-    cursor.close()
-    db.close()
+# The code should not be executed when imported
+if __name__ == '__main__':
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
