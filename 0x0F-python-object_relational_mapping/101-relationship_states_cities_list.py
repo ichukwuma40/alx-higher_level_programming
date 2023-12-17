59ebad0583935f6fd0f7c85c1309caa53730bdef
Innocent Chukwuma <ichukwuma40@gmail.com>
2023-12-17 01:33:22 -0800

    object relational mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" To print the State object with the name passed as argument from the database
 """
 import sys
 from relationship_state import Base, State
@@ -10,6 +10,7 @@ from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
@@ -20,4 +21,3 @@ if __name__ == "__main__":
         for city_ins in instance.cities:
             print("    ", end="")
             print(city_ins.id, city_ins.name, sep=": ")
-
